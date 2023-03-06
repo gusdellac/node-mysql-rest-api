2cb005c48e3456ee564b7382b08c52f60a1a518f
Gustavo DeLlac <gustavodellac@gmail.com>
2023-03-05 23:17:08 -0300

    dotenv module, .env , config.js, app.js , .gitignore, script start package.json , modif employees controller, modif employees routes, modif db.js , modif index.js, modif db.js

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// const express = require("express"); //modulos de common js de node
+import express from "express"; //modulos ES modules de node, codigo moderno
+import employeesRoutes from "./routes/employees.routes.js";
+import indexRoutes from "./routes/index.routes.js";
+
+const app = express();
+
+app.use(express.json()); //metodo express.json() convierte la info recibida en formato JSON a un objeto de javascript para que este
+//pueda interpretarlo y utilizarlo. De esta forma indicamos a app que traduzca la info antes de pasarlas a las rutas
+
+//instalamos nodemon como dependecia de desarrollo (npm i nodemon -D), ejecutamos con npm run dev
+
+app.use(indexRoutes);
+app.use("/api",employeesRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "endpoint not found"
+    });
+});
+
+export default app;
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -11,7 +11,7 @@ router.get("/employees/:id", getEmployee);
 
 
 router.post("/employees", createEmployee);
-router.put("/employees", updateEmployee);
+router.patch("/employees/:id", updateEmployee);
 router.delete("/employees/:id", deleteEmployee);
 
 export default router
